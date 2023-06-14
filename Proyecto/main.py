eaad57295202e04b96977b0d5ac735f625dab560
Miguiiii <miguel.alejandro.menendez@gmail.com>
2023-06-14 00:29:11 -0400

    Update main.py

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,4 +1,5 @@
-import os, requests, json, gestionClientes, gestionEnvios, gestionEstadisticas, gestionPagos, gestionProductos, gestionVentas
+import os, requests, json
+import gestionProductos, gestionVentas, gestionClientes, gestionPagos, gestionEnvios, gestionEstadisticas
 
 def borrado_datos():
     for i in ["Productos.txt", "Clientes.txt", "Envios.txt", "Ventas.txt", "Pagos.txt", "Estadisticas.txt"]:
@@ -14,7 +15,7 @@ def main():
     pre_cargado()
     while True:
         print(
-            "Bienvenido al sistema en línea de la tienda de productos naturales.\n"
+            "Bienvenido al sistema en línea de la tienda de productos naturales\n"
             "1.- Gestionar productos\n"
             "2.- Gestionar ventas\n"
             "3.- Gestionar clientes\n"
@@ -22,31 +23,56 @@ def main():
             "5.- Gestionar envíos\n"
             "6.- Visualizar estadísticas\n"
             "7.- Reestablecer el estado inicial del programa\n"
-            "8.- Salir"
+            "8.- Salir\n"
               )
         while True:
             opcion=input("Ingrese el número de la acción a realizar: ")
             if opcion not in ["1", "2", "3", "4", "5", "6", "7", "8"]:
-                print("ADVERTENCIA: Por favor ingrese un número de acción válido.")
+                print("ADVERTENCIA: Por favor ingrese un número de acción válido".center(67, "*"))
                 continue
             break
         if opcion=="1":
-            continue
+            print(
+                "1.- Agregar un nuevo producto\n"
+                "2.- Buscar productos en la base de datos\n"
+                "3.- Modificar información de productos existentes\n"
+                "4.- Eliminar productos de la tienda\n"
+            )
         
         elif opcion=="2":
-            continue
+            print(
+                "1.- Registrar una nueva venta\n"
+                "2.- Generar factura de compra\n"
+                "3.- Buscar ventas en la base de datos\n"
+            )
 
         elif opcion=="3":
-            continue
+            print(
+                "1.- Registrar un nuevo cliente\n"
+                "2.- Buscar clientes en la base de datos\n"
+                "3.- Modificar información de clientes existentes\n"
+                "4.- Eliminar clientes de la tienda\n"
+            )
 
         elif opcion=="4":
-            continue
+            print(
+                "1.- Registrar un nuevo pago\n"
+                "2.- Buscar pagos en la base de datos\n"
+            )
 
         elif opcion=="5":
-            continue
+            print(
+                "1.- Registrar un nuevo envío\n"
+                "2.- Buscar envíos en la base de datos\n"
+            )
 
         elif opcion=="6":
-            continue
+            print(
+                "1.- Estadísticas de ventas\n"
+                "2.- Estadísticas de pagos\n"
+                "3.- Estadísticas de envíos\n"
+                "4.- Generar gráficos de estadísticas\n"
+            )
 
         elif opcion=="7":
             while True:
